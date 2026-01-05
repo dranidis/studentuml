a5dcffc14ae6b46ad35c76b1f3fd4afbec309e29
D Dranidis <dranidis@gmail.com>
2026-01-05 14:09:31 +0200

    Restore commented out edit in ActorInstanceGR

--- src/main/java/edu/city/studentuml/model/graphical/ActorInstanceGR.java
+++ src/main/java/edu/city/studentuml/model/graphical/ActorInstanceGR.java
@@ -5,13 +5,25 @@ import java.awt.font.FontRenderContext;
 import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
 
+import javax.swing.JOptionPane;
+import javax.swing.undo.CompoundEdit;
+
 import org.w3c.dom.Element;
 
 import edu.city.studentuml.editing.EditContext;
+import edu.city.studentuml.model.domain.Actor;
 import edu.city.studentuml.model.domain.ActorInstance;
+import edu.city.studentuml.model.repository.CentralRepository;
 import edu.city.studentuml.util.NotStreamable;
+import edu.city.studentuml.util.SystemWideObjectNamePool;
 import edu.city.studentuml.util.XMLStreamer;
+import edu.city.studentuml.util.undoredo.ActorInstanceEdit;
+import edu.city.studentuml.util.undoredo.ActorRepositoryOperations;
+import edu.city.studentuml.util.undoredo.EditActorInstanceEdit;
+import edu.city.studentuml.util.undoredo.TypeRepositoryOperations;
 import edu.city.studentuml.view.gui.ActorInstanceEditor;
+import edu.city.studentuml.view.gui.TypeOperation;
+import edu.city.studentuml.view.gui.TypedEntityEditResult;
 
 /**
  * @author Ervin Ramollari
@@ -73,58 +85,83 @@ public class ActorInstanceGR extends AbstractSDObjectGR {
      *                component
      * @return true if editing was successful or cancelled, false if there was an
      *         error
-     * @Override public boolean edit(EditContext context) { CentralRepository
-     *           repository = context.getModel().getCentralRepository();
-     *           ActorInstance originalActorInstance = getActorInstance(); // Create
-     *           editor and initial result ActorInstanceEditor actorInstanceEditor =
-     *           createEditor(context); TypedEntityEditResult<Actor, ActorInstance>
-     *           initialResult = new TypedEntityEditResult<>(originalActorInstance,
-     *           new java.util.ArrayList<>()); TypedEntityEditResult<Actor,
-     *           ActorInstance> result =
-     *           actorInstanceEditor.editDialog(initialResult,
-     *           context.getParentComponent()); // Check if user cancelled if
-     *           (result == null) { return true; } ActorInstance newActorInstance =
-     *           result.getDomainObject(); // UNDO/REDO setup ActorInstance
-     *           undoActorInstance = originalActorInstance.clone();
-     *           ActorInstanceEdit undoEdit = new
-     *           ActorInstanceEdit(undoActorInstance,
-     *           originalActorInstance.getActor().getName()); // Create compound
-     *           edit for all operations CompoundEdit compoundEdit = new
-     *           CompoundEdit(); // Apply type operations first and add their undo
-     *           edits TypeRepositoryOperations<Actor> typeOps = new
-     *           ActorRepositoryOperations(); for (TypeOperation<Actor> typeOp :
-     *           result.getTypeOperations()) {
-     *           typeOp.applyTypeOperationsAndAddTheirUndoEdits(repository, typeOps,
-     *           compoundEdit); } // edit the actor if there is no change in the
-     *           name, // or if there is a change in the name but the new name
-     *           doesn't bring any // conflict // or if the new name is blank if
-     *           (!originalActorInstance.getName().equals(newActorInstance.getName())
-     *           && repository.getActorInstance(newActorInstance.getName()) != null
-     *           && !newActorInstance.getName().equals("")) { int response =
-     *           JOptionPane.showConfirmDialog(null, "There is an existing actor
-     *           instance with the given name already.\n" + "Do you want this
-     *           diagram actor instance to refer to the existing one?", "Warning",
-     *           JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE); if
-     *           (response == JOptionPane.YES_OPTION) {
-     *           setActorInstance(repository.getActorInstance(newActorInstance.getName()));
-     *           if (originalActorInstance.getName().equals("")) {
-     *           repository.removeActorInstance(originalActorInstance); } } } else {
-     *           repository.editActorInstance(originalActorInstance,
-     *           newActorInstance); // Add domain object edit to compound
-     *           ActorInstanceEdit originalEdit = new
-     *           ActorInstanceEdit(originalActorInstance,
-     *           originalActorInstance.getActor().getName());
-     *           compoundEdit.addEdit(new EditActorInstanceEdit(originalEdit,
-     *           undoEdit, context.getModel())); } // Post the compound edit
-     *           compoundEdit.end(); if (!compoundEdit.isInProgress() &&
-     *           compoundEdit.canUndo()) {
-     *           context.getParentComponent().getUndoSupport().postEdit(compoundEdit);
-     *           } // set observable model to changed in order to notify its views
-     *           context.getModel().modelChanged();
-     *           SystemWideObjectNamePool.getInstance().reload(); return true; } /**
-     *           Creates the editor for this Actor Instance. Extracted into a
-     *           protected method to enable testing without UI dialogs (can be
-     *           overridden to return mock editor).
+     */
+    @Override
+    public boolean edit(EditContext context) {
+        CentralRepository repository = context.getModel().getCentralRepository();
+        ActorInstance originalActorInstance = getActorInstance();
+
+        // Create editor and initial result
+        ActorInstanceEditor actorInstanceEditor = createEditor(context);
+        TypedEntityEditResult<Actor, ActorInstance> initialResult = new TypedEntityEditResult<>(
+                originalActorInstance, new java.util.ArrayList<>());
+        TypedEntityEditResult<Actor, ActorInstance> result = actorInstanceEditor.editDialog(initialResult,
+                context.getParentComponent());
+
+        // Check if user cancelled
+        if (result == null) {
+            return true;
+        }
+
+        ActorInstance newActorInstance = result.getDomainObject();
+
+        // UNDO/REDO setup
+        ActorInstance undoActorInstance = originalActorInstance.clone();
+        ActorInstanceEdit undoEdit = new ActorInstanceEdit(undoActorInstance,
+                originalActorInstance.getActor().getName());
+
+        // Create compound edit for all operations
+        CompoundEdit compoundEdit = new CompoundEdit();
+
+        // Apply type operations first and add their undo edits
+        TypeRepositoryOperations<Actor> typeOps = new ActorRepositoryOperations();
+        for (TypeOperation<Actor> typeOp : result.getTypeOperations()) {
+            typeOp.applyTypeOperationsAndAddTheirUndoEdits(repository, typeOps, compoundEdit);
+        }
+
+        // edit the actor if there is no change in the name,
+        // or if there is a change in the name but the new name doesn't bring any
+        // conflict
+        // or if the new name is blank
+        if (!originalActorInstance.getName().equals(newActorInstance.getName())
+                && repository.getActorInstance(newActorInstance.getName()) != null
+                && !newActorInstance.getName().equals("")) {
+            int response = JOptionPane.showConfirmDialog(null,
+                    "There is an existing actor instance with the given name already.\n"
+                            + "Do you want this diagram actor instance to refer to the existing one?",
+                    "Warning", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
+            if (response == JOptionPane.YES_OPTION) {
+                setActorInstance(repository.getActorInstance(newActorInstance.getName()));
+                if (originalActorInstance.getName().equals("")) {
+                    repository.removeActorInstance(originalActorInstance);
+                }
+            }
+        } else {
+            repository.editActorInstance(originalActorInstance, newActorInstance);
+            // Add domain object edit to compound
+            ActorInstanceEdit originalEdit = new ActorInstanceEdit(originalActorInstance,
+                    originalActorInstance.getActor().getName());
+            compoundEdit.addEdit(new EditActorInstanceEdit(originalEdit, undoEdit, context.getModel()));
+        }
+
+        // Post the compound edit
+        compoundEdit.end();
+        if (!compoundEdit.isInProgress() && compoundEdit.canUndo()) {
+            context.getParentComponent().getUndoSupport().postEdit(compoundEdit);
+        }
+
+        // set observable model to changed in order to notify its views
+        context.getModel().modelChanged();
+        SystemWideObjectNamePool.getInstance().reload();
+
+        return true;
+    }
+
+    /**
+     * Creates the editor for this Actor Instance. Extracted into a protected method
+     * to enable testing without UI dialogs (can be overridden to return mock
+     * editor).
+     * 
      * @param context the edit context containing repository
      * @return the editor instance
      */
